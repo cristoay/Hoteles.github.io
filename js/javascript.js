6a15fd565be1528353ca45de6ceabfc33ddef9ff
cristoay <cristo.ay@gmail.com>
2020-12-04 17:41:09 +0100

    Terminado

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,14 @@ var hoy = new Date();
 var dd = hoy.getDate();
 var mm = hoy.getMonth() + 1;
 var yyyy = hoy.getFullYear();
-if (dd < 10)
-    dd = "0" + dd;
-if (mm < 10)
-    mm = "0" + mm;
+if (dd < 10) {
+  dd = '0' + dd;
+}
+    
+if (mm < 10) {
+    mm = '0' + mm;
+}
+    
 var prueba = yyyy + "-" + mm + "-" + dd;
 var fechaInput = document.getElementById("fecha");
 document.getElementById("buscar").addEventListener("click", buscar);
